db1642ec938232d17460eff46fc449ea2f399113
herryam <herryanugerah1@gmail.com>
2022-02-03 20:29:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './Landing';
+
+
+export default function App() {
+	return (
+		<div>
+			<Landing />
+		</div>
+	);
+}
---
+++ src/Landing/HeroImage/index.js
@@ -0,0 +1,1 @@
+// Kode di sini
