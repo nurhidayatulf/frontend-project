94127afe3ef791782dc828613e54511ab6997280
herryam <herryanugerah1@gmail.com>
2022-02-18 15:29:04 +0700

    Class Detail

--- src/Kelas/index.js
+++ src/Kelas/index.js
@@ -2,8 +2,10 @@ import './index.css';
 import { Row, Col, Container, Card, Badge } from 'react-bootstrap';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-import { BsFillStarFill, BsFillCheckCircleFill } from "react-icons/bs";
+import { BsFillStarFill, BsFillCheckCircleFill, BsStar } from "react-icons/bs";
 import Helmet from "react-helmet";
+import { FaFacebookF, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { VscArrowRight } from 'react-icons/vsc';
 
 export default function Kelas() {
 
@@ -34,6 +36,19 @@ export default function Kelas() {
     return (
         <div>
             <Header />
+            <div class="hero-cover">
+                <img src={require('../Assets/hero2.jpg')} alt="hero2" class="hero2" />
+                <div class="transparan">
+                    <p class="text-white fs-2 fw-bolder ms-5 ps-5 mt-5 pt-4">Figma for UI/UX: Master Web<br />Design in Figma</p>
+                    <BsFillStarFill className='text-warning star-margin star2 me-2'/>
+                    <BsFillStarFill className='text-warning me-2 star2' />
+                    <BsFillStarFill className='text-warning me-2 star2' />
+                    <BsFillStarFill className='text-warning me-2 star2' />
+                    <BsStar className='text-warning star2' />
+                    <p class="text-white fw-bold ms-5 ps-5 mt-5">A solution for easy and flexible online learning, you can<br />study anywhere through this platform</p>
+                    <button type="button" class="btn btn-primary rounded-pill fw-bold mt-5 create-class"><span class="me-2">Create Class</span><VscArrowRight className="arrow rounded-circle" /></button>
+                </div>
+            </div>
             <div class="mt-5">
                 <Container>
                     <Card bg="primary text-white">
@@ -41,9 +56,23 @@ export default function Kelas() {
                             Description
                         </Card.Header>
                         <Card.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus pharetra, sed rhoncus, adipiscing. Mi, mi, nisi, morbi ullamcorper. Maecenas sed odio mi quis consequat convallis sapien aliquam. Feugiat ut aliquam elit bibendum elementum felis. Sodales magna ullamcorper orci eleifend pretium. Pretium auctor odio donec hendrerit tempor placerat. Euismod convallis urna convallis cum sit a imperdiet. Scelerisque ac pellentesque commodo ornare eget nam duis sit lectus. Volutpat, orci vel sem varius sit aenean viverra. Malesuada risus lobortis quisque quis pharetra donec nibh.
+                            <p class="testy ms-3 mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus pharetra, sed rhoncus, adipiscing. Mi, mi, nisi, morbi ullamcorper. Maecenas sed odio mi quis consequat convallis sapien aliquam. Feugiat ut aliquam elit bibendum elementum felis. Sodales magna ullamcorper orci eleifend pretium. Pretium auctor odio donec hendrerit tempor placerat. Euismod convallis urna convallis cum sit a imperdiet. Scelerisque ac pellentesque commodo ornare eget nam duis sit lectus. Volutpat, orci vel sem varius sit aenean viverra. Malesuada risus lobortis quisque quis pharetra donec nibh.</p>
                         </Card.Body>
                     </Card>
+                </Container>
+            </div>
+            <div class="mt-5">
+                <Container>
+                    <p class="text-primary fs-5 fw-bolder mb-4">About Mentor</p>
+                    <div class="d-flex">
+                        <img src={require('../Assets/mentor2.jpg')} alt="mentor" class="rounded-circle" />
+                        <div class="ms-5">
+                            <p class="text-primary mentor-name mb-2">Steve Jobs</p>
+                            <p class="text-secondary testy mentor-status">CEO Apple</p>
+                            <span><FaFacebookF class="fb bg-primary rounded-circle me-2" /> <FaLinkedin class="linke bg-primary rounded-circle me-2"/> <FaInstagram class="linke bg-primary rounded-circle"/></span>
+                        </div>
+                    </div>
+                    <div class="fw-bold testy mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus pharetra, sed rhoncus, adipiscing. Mi, mi, nisi, morbi ullamcorper. Maecenas sed odio mi quis consequat convallis sapien aliquam.</div>
                 </Container>
             </div>
             <div class="mt-5">
